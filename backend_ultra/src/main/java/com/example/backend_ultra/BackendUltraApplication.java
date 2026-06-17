93444213b6f5549153d5139aa28409a462b1cd51
EunSSeob <163013769+Eunsseob@users.noreply.github.com>
2026-06-17 15:52:32 +0900

    feat. 백엔드 생성

---
+++ backend_ultra/src/main/java/com/example/backend_ultra/BackendUltraApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend_ultra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendUltraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendUltraApplication.class, args);
+    }
+
+}
---
+++ backend_ultra/src/test/java/com/example/backend_ultra/BackendUltraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend_ultra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendUltraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
